fb318a15b6f8bba338ba28af5f3a76aa2328e31b
Kim David <63450340+PowerStream3604@users.noreply.github.com>
2021-02-03 13:42:48 +0900

    Add files via upload

---
+++ Gmail.java
@@ -0,0 +1,14 @@
+
+package email.verification;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class Gmail extends Authenticator{
+	
+	@Override
+	protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication("your_email_address","your_email_password");
+	}
+	
+}
